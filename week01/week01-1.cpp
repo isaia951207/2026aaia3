ed9d91ff39e437183a584703977691b880c11cd8
isaia951207 <125284308+isaia951207@users.noreply.github.com>
2026-09-09 11:00:23 +0800

    Add files via upload

---
+++ week01/week01-1.cpp
@@ -0,0 +1,19 @@
+// week01-1.cpp SOIT106_ADVANCE_001
+#include <stdio.h>
+int main()
+{
+    int N; /// 整數 N
+    scanf("%d", &N); /// C語言，讀入整數N
+    int b = N, ans=0; /// 把 N 的值備份b，答案0開始湊
+    while(N>0){ /// 只要 N 還在，就繼續「剝皮」
+        ans = ans*10 + N%10; /// 答案 * 10 + 皮皮
+        N = N / 10; /// N 被剝皮
+    }
+    printf("%d+%d=%d\n", b, ans, b+ans);
+}/// 1234
+///     4 剝皮
+///  123 剝完皮，變小
+///     3
+///   12
+///    2
+///    1
